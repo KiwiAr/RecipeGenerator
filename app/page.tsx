4c3d1105e54d2bc771b31fa697228ab39fa26f2d
mayuanxin1234 <95178026+mayuanxin1234@users.noreply.github.com>
2023-05-29 23:03:55 +0800

    Updated UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,94 +1,49 @@
-import Image from 'next/image'
+import Image from 'next/image';
 import styles from './page.module.css'
+
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <div className={styles.heading}>
+        <Image src = "/KiwiLogo.png" width={100} height={50} alt = 'kiwilogo'/>
+        <h1>KiwiAr</h1>
+      </div>
+      <div>
+        <h2 className={styles.recipeheading}>Welcome to the recipe generator!</h2>
+      </div>
+      <div>
+      <form action="/send-data-here" method="post">
+        <label className={styles.input}>Input:</label>
+        <input type="text" id="first" name="first" className = {styles.inputBox} 
+        placeholder = "Type here or select the suggested categories below..."/>
+        <button type="submit" className={styles.submitButton}>Submit</button>
+      </form>
         </div>
+        <div className = {styles.input}>
+        <h3>Suggested:</h3>
+        <button className = {styles.submitButton}>Party</button>
+        <button className = {styles.submitButton}>Meal-Prep</button>
+        <button className = {styles.submitButton}>Family Cooking</button>
+        </div>
+        <div>
+        <h3>Recommended Items:</h3>
+        </div>
+        <div>
+        <p>Click on each item to find <br/>
+        out more</p>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div>
+        <h3>Recipe: </h3>
+        <button className = {styles.submitButton}>Buy on Shopee now!</button>
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div>
+        <form action="/send-data-here" method="post">
+        <label className={styles.input}>Not what you like? <br/> Enter tweaks here:</label>
+        <input type="text" id="first" name="first" className = {styles.inputBox} 
+        placeholder = "Type here or select the suggested categories below..."/>
+        <button type="submit" className={styles.submitButton}>Submit</button>
+      </form>
       </div>
     </main>
   )
